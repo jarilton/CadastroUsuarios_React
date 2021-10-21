cd2c561ae78fc3eac9512bc9e7500b632bcc32b6
jarilton <jariltonpereira02@hotmail.com>
2021-10-21 19:35:06 -0300

    implements Footer

--- frontend/src/components/template/Footer.jsx
+++ frontend/src/components/template/Footer.jsx
@@ -3,5 +3,8 @@ import React from 'react'
 
 export default props =>
     <footer className="footer">
-        Rodapé
+        <span>
+            Desenvolvido com am<i className="fa fa-heart text-danger"></i>r por
+            <strong> Jamal Dev de Software</strong>
+        </span>
     </footer>
